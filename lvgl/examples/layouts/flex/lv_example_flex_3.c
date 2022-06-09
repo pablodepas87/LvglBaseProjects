4b4668dbead1cfb36d5b920da5fe3adc50a7d233
Fabio Rapicano <rappi0787@outlook.it>
2022-06-09 11:48:52 +0200

    layout flex example 3 e 4
    Signed-off-by: Fabio Rapicano <rappi0787@outlook.it>

--- lvgl/examples/layouts/flex/lv_example_flex_3.c
+++ lvgl/examples/layouts/flex/lv_example_flex_3.c
@@ -7,8 +7,8 @@
 void lv_example_flex_3(void)
 {
     lv_obj_t * cont = lv_obj_create(lv_scr_act());
-    lv_obj_set_size(cont, 300, 220);
-    lv_obj_center(cont);
+    lv_obj_set_size(cont, 300, 100);
+    lv_obj_align(cont, LV_ALIGN_TOP_MID, 0, 10);
     lv_obj_set_flex_flow(cont, LV_FLEX_FLOW_ROW);
 
     lv_obj_t * obj;
